bd4939aaf8506e708d01edf62d960f73611e785d
Monster <coolpest@gmail.com>
2020-05-20 06:41:58 +0800

    pass parameter

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
 const decoder = new TextDecoder("utf-8");
-  const data = await Deno.readFile("./user.json");
-  console.log(decoder.decode(data));
+const data = await Deno.readFile("./user.json");
+console.log(decoder.decode(data));
+
+var name:string
+function ShowName(name:string){
+    console.log(` 大爷 ${name}，你好啊~`)
+}
+ShowName("老王")
