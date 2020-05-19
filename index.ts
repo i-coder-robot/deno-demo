24fa89137ff68734185847c96f3f908db9e1bfff
Monster <coolpest@gmail.com>
2020-05-20 06:57:17 +0800

    getuser

--- index.ts
+++ index.ts
@@ -7,3 +7,28 @@ function ShowName(name:string){
     console.log(` 大爷 ${name}，你好啊~`)
 }
 ShowName("老王")
+
+import {Application,Router} from "https://deno.land/x/oak/mod.ts"
+const router = new Router();
+router.get("/",(context:any)=>{
+  context.response.body="Hello Deno"
+})
+
+router.get("/getUser",(context:any)=>{
+  try{
+    const decoder = new TextDecoder("utf-8")
+    const data = Deno.readFileSync("./user.json")
+    console.log(decoder.decode(data))
+    context.response.body = decoder.decode(data)
+  }catch(err){
+    console.log(err)
+    context.response.status=500;
+    context.response.body=err
+  }
+})
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+console.log("服务器地址是 http://localhost:8888")
+await app.listen({port:8888})
